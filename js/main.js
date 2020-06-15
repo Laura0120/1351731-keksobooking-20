851a95ba05aa2ef5e772c5e7fb594f76d9647b7e
a_galustyan <a_galustyan@head-point.ru>
2020-06-15 20:12:35 +0300

    Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -20,13 +20,75 @@ var DESCRIPTION = ['добро пожаловать!'];
 
 var announcements = [];
 var map = document.querySelector('.map');
-
+var mark = document.querySelector('.map__pin--main');
 var pinsContainer = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+var adForm = document.querySelector('.ad-form');
+var fieldsetsAdForm = adForm.querySelectorAll('fieldset');
+var roomNumber = adForm.querySelector('#room_number');
+var numberOfGuests = adForm.querySelector('#capacity');
+
+var handleRoomNumberChange = function () {
+  for (var i = 0; i < numberOfGuests.options.length; i++) {
+    var guestsOption = numberOfGuests.options[i];
+    if (roomNumber.value === '100') {
+      guestsOption.disabled = guestsOption.value > '0';
+    } else if (guestsOption.value <= roomNumber.value && guestsOption.value !== '0') {
+      guestsOption.disabled = false;
+    } else {
+      guestsOption.disabled = true;
+    }
+  }
+
+  var selectedOption = numberOfGuests.selectedOptions[0];
+  if (selectedOption.disabled === true) {
+    numberOfGuests.setCustomValidity('выбрано недопустимое значение');
+  } else {
+    numberOfGuests.setCustomValidity('');
+  }
+};
+
+handleRoomNumberChange();
+roomNumber.addEventListener('change', handleRoomNumberChange);
+numberOfGuests.addEventListener('change', function () {
+  var selectedOption = numberOfGuests.selectedOptions[0];
+  if (selectedOption.disabled === false) {
+    numberOfGuests.setCustomValidity('');
+  }
+});
+
+var addDisabled = function () {
+  for (var i = 0; i < fieldsetsAdForm.length; i++) {
+    fieldsetsAdForm[i].disabled = true;
+  }
+  return fieldsetsAdForm;
+};
+
+addDisabled();
+
+var cancelDisabled = function () {
+  for (var i = 0; i < fieldsetsAdForm.length; i++) {
+    fieldsetsAdForm[i].disabled = false;
+  }
+  return fieldsetsAdForm;
+};
+
+var getAddress = function () {
+  var addressInput = adForm.querySelector('#address');
+  addressInput.value = Math.round(mark.offsetLeft + PIN_WIDTH / 2) + ', ' + Math.round(mark.offsetTop + PIN_HEIGHT);
+  addressInput.disabled = true;
+};
+
+getAddress();
+
 var showMap = function () {
   map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  cancelDisabled();
+  renderPinElements();
 };
+
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -96,5 +158,14 @@ var renderPinElements = function () {
 
 announcements = createAnnouncements();
 
-showMap();
-renderPinElements();
+mark.addEventListener('mousedown', function (evt) {
+  if (evt.which === 1) {
+    showMap();
+  }
+});
+
+mark.addEventListener('keydown', function (evt) {
+  if (evt.code === 'Enter') {
+    showMap();
+  }
+});
