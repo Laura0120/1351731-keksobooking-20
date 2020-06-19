1505af200bf9e46b4af53273b695b39b6bf29323
a_galustyan <a_galustyan@head-point.ru>
2020-06-19 16:27:05 +0300

    исправления по замечаниям 3

--- js/data.js
+++ js/data.js
@@ -20,7 +20,7 @@
     var announcements = [];
     for (var i = 1; i <= ANNOUNCEMENTS_COUNT; i++) {
       var location = {
-        x: window.util.getRandomValue(0, getWidth()),
+        x: window.util.getRandomValue(0, window.map.getWidth),
         y: window.util.getRandomValue(130, 630),
       };
       announcements.push({
